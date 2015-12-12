e985eeefd93875d8369ac4ce35ca83bb197e2271
Antonio Culler <aculler47@gmail.com>
2015-12-12 04:03:04 -0500

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String fizz = "Fizz";
+        String buzz = "Buzz";
+        String fizzBuzz = "FizzBuzz";
+
+        int fizzCount = 0;
+        int buzzCount = 0;
+        int fbCount = 0;
+
+        for (int i = 1; i < 101; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)){
+                System.out.println(fizzBuzz);
+                fbCount++;
+            }
+
+            else if (i % 3 == 0){
+                System.out.println(fizz);
+                fizzCount++;
+            }
+
+            else if (i % 5 == 0){
+                System.out.println(buzz);
+                buzzCount++;
+            }
+
+            else {
+                System.out.println(i);
+            }
+
+        }
+
+        System.out.println("f :" + fizzCount + " b: " + buzzCount + " fb: " + fbCount);
+    }
+}
